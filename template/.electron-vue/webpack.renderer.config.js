2a88c349d3b6a68ffb761739e3f222ba09aa36a9
Cyvelnet <Cyvelnet@gmail.com>
2021-02-03 16:22:09 +0800

    Update webpack.renderer.config.js

--- template/.electron-vue/webpack.renderer.config.js
+++ template/.electron-vue/webpack.renderer.config.js
@@ -129,10 +129,17 @@ let rendererConfig = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, '../src/index.ejs'),
-      minify: {
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        removeComments: true
+      templateParameters(compilation, assets, options) {
+        return {
+          compilation: compilation,
+          webpack: compilation.getStats().toJson(),
+          webpackConfig: compilation.options,
+          htmlWebpackPlugin: {
+            files: assets,
+            options: options,
+          },
+          process,
+        };
       },
       nodeModules: process.env.NODE_ENV !== 'production'
         ? path.resolve(__dirname, '../node_modules')
