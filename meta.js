3eb8b1c8b758e276c8eb65303262539e76ca9174
Cyvelnet <Cyvelnet@gmail.com>
2021-02-03 16:49:00 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -149,7 +149,7 @@ module.exports = {
         'vue-electron': '^1.0.6',
         'vue-router': '^3.0.1',
         'vuex': '^3.0.1',
-        'electron-vuex': '^1.0.5'
+        'electron-vuex': '^1.0.3'
       }
 
       if (Object.keys(plugins).length > 0) output += ',\n'
