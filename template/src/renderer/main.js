7909890ad6f97e7f5401b10df6ab0e5b71fb2ccf
Cyvelnet <Cyvelnet@gmail.com>
2021-02-05 12:38:54 +0800

    Update main.js

--- template/src/renderer/main.js
+++ template/src/renderer/main.js
@@ -22,9 +22,7 @@ Vue.http = Vue.prototype.$http = axios
 import ElementUi from 'element-ui';
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUi);
-
 {{/isEnabled}}
-  
   
 Vue.config.productionTip = false
 
